4e939878bdd65926a5211ab18bcf6c67a815ba1e
Jordan Deuley <114099341+dewy413@users.noreply.github.com>
2023-03-16 02:35:30 -0500

    done did

---
+++ Spring '23/Programming Seminar/Programs/Program8/Jordan_Deuley_Program_8.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+int main() {
+
+    int miles, mins, secs;
+    string pace;
+    pace = split(pace);
+
+
+    cout << "Enter distance in miles:";
+    cin >> miles;
+    cout << "Enter your targe pace:";
+    cin >> pace;
+
+
+    for(int i = 0; i < miles; i++) {
+
+    }
+
+}
